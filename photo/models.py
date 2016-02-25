ef97b8f7f30197329b379e1cf06d84025b80f6be
jaeng lee <jeansvvv@gmail.com>
2016-02-25 22:55:44 +0900

    photo 모델 정의

--- photo/models.py
+++ photo/models.py
@@ -1,5 +1,12 @@
+# coding: utf-8
+
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Photo(modles.Model):
+	image_file = models.ImageField()
+	filtered_image_file = models.ImageField()
+	description = models.TextField(max_length=500)
+	created_at = models.DateTimeField(auto_now_add=True)
