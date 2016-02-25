c31216edc14ab7d7d54fdd06cf7fb1ded6931070
jaeng lee <jeansvvv@gmail.com>
2016-02-25 23:32:57 +0900

    포토 모델에 description에 blank가 가능하도록 변경, 그리고 migration

--- photo/models.py
+++ photo/models.py
@@ -8,5 +8,5 @@ from django.db import models
 class Photo(models.Model):
 	image_file = models.ImageField()
 	filtered_image_file = models.ImageField()
-	description = models.TextField(max_length=500)
+	description = models.TextField(max_length=500, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
