35c843b8e99c9ea02169ca4f3f12d299b8803ed0
jaeng lee <jeansvvv@gmail.com>
2016-02-25 22:58:10 +0900

    오타 수정

--- photo/models.py
+++ photo/models.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Photo(modles.Model):
+class Photo(models.Model):
 	image_file = models.ImageField()
 	filtered_image_file = models.ImageField()
 	description = models.TextField(max_length=500)
