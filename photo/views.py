5bc85d7f79c59d609a4671da828ef129eebbf6c5
jaeng lee <jeansvvv@gmail.com>
2016-03-01 16:01:57 +0900

    /photo/<photo_id> 라는 URL로 접근을 하면 photo_id를 parameter로 받도록 구현

--- photo/views.py
+++ photo/views.py
@@ -3,5 +3,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def single_photo(request):
-    return HttpResponse('3번 사진')
+def single_photo(request, photo_id):
+    return HttpResponse('{0}번 사진을 보여드릴게요.'.format(photo_id))
