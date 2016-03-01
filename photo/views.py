d214e3b055973b0916d6d1add1fa251001a7ef1a
jaeng lee <jeansvvv@gmail.com>
2016-03-01 16:28:11 +0900

    /photo/<photo_id> 로 접근했을 때, 사진을 출력해주도록 views.py 파일 수정

--- photo/views.py
+++ photo/views.py
@@ -1,7 +1,25 @@
 # coding: utf-8
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Photo
 
 def single_photo(request, photo_id):
-    return HttpResponse('{0}번 사진을 보여드릴게요.'.format(photo_id))
+    """
+    Try Catch 문도 쓸 수 있음
+    try:
+        photo = Photo.objects.get(pk=photo_id)
+    except Photo.DoesNotExist:
+        return HttpResponse("No Photo")
+    """
+    photo = get_object_or_404(Photo, pk=photo_id)
+
+    response_text = '<p>{photo_id}번 사진</p>'
+    response_text += '<p><image src="{photo_url}"/></p>'
+
+    return HttpResponse(response_text.format(
+        photo_id=photo_id,
+        photo_url=photo.image_file.url
+        )
+    )
+
